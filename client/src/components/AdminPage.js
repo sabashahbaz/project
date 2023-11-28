165f9318130bd01367fc456fc5218336baa8f0d9
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-11-28 12:29:59 -0500

    table with complaints

--- client/src/components/AdminPage.js
+++ client/src/components/AdminPage.js
@@ -1,36 +1,4 @@
 import React, {useState, useEffect} from 'react'
-import { Card, Typography } from "@material-tailwind/react";
-//import axios from 'axios'
-
-const TABLE_HEAD = ["Name", "Email", "Complaint", "Status"];
-    
-    // const TABLE_ROWS = [
-    // {
-    //     name: "John Michael",
-    //     job: "Manager",
-    //     date: "23/04/18",
-    // },
-    // {
-    //     name: "Alexa Liras",
-    //     job: "Developer",
-    //     date: "23/04/18",
-    // },
-    // {
-    //     name: "Laurent Perrier",
-    //     job: "Executive",
-    //     date: "19/09/17",
-    // },
-    // {
-    //     name: "Michael Levi",
-    //     job: "Developer",
-    //     date: "24/12/08",
-    // },
-    // {
-    //     name: "Richard Gran",
-    //     job: "Manager",
-    //     date: "04/10/21",
-    // },
-    // ];
 
 function  AdminPage () {
 
@@ -50,26 +18,41 @@ function  AdminPage () {
     
 
     return (
-        <table class="table-auto">
-        <thead>
-            <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Description</th>
-            <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            {userInfo.map(user => (
-            <tr key={user.id}>
-                <td>{user.first_name} {user.last_name}</td>
-                <td>{user.email}</td>
-                <td>{user.description}</td>
-                <td>{user.status}</td>
-            </tr>
-        ))}
-        </tbody>
-        </table>
+        <div className="flex items-center justify-center h-screen">
+            <div class="flex flex-col">
+                <div class="-m-1.5 overflow-x-auto">
+                    <div class="p-1.5 min-w-full inline-block align-middle">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                        <thead>
+                            <tr>
+                            <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
+                            <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Email</th>
+                            <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Complaint</th>
+                            <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                            {userInfo.map(user => (
+                                <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{user.first_name} {user.last_name}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{user.email}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{user.description}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{user.status}</td>
+                            {/* <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Delete</button>
+                            </td> */}
+                            </tr>
+
+                            ))}
+                        </tbody>
+                        </table>
+                    </div>
+                    </div>
+                </div>
+                </div>
+        </div>
+        
     )
     }
 export default AdminPage 
